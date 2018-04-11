9a42a52064e3ea7dda262e500901167a9c871ec4
Emehinola Idowu <hydeenoble39@gmail.com>
2018-04-11 08:09:28 +0100

    Add to remove the sections i generated after discovering there we adding an extra html tagto the site structure and the flex properties would not affect the elementes i want

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
-import { HotelViewComponent } from './hotel-view/hotel-view.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideBarComponent,
-    HotelViewComponent
   ],
   imports: [
     BrowserModule
